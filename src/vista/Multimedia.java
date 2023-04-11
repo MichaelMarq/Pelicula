31c39c75b29bc25ea47d6951b1bbd8075db21655
MichaelMarq <michael-marquez@outlook.com>
2023-04-11 18:16:25 -0500

    Programa terminado

---
+++ src/vista/Multimedia.java
@@ -0,0 +1,352 @@
+
+package vista;
+
+
+public class Multimedia extends javax.swing.JFrame {
+
+//Constructor
+    public Multimedia() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        JScroll1 = new javax.swing.JScrollPane();
+        JPanel = new javax.swing.JPanel();
+        JLfoto1 = new javax.swing.JLabel();
+        JLtitulo1 = new javax.swing.JLabel();
+        JLgenero1 = new javax.swing.JLabel();
+        JLfoto2 = new javax.swing.JLabel();
+        JLtitulo2 = new javax.swing.JLabel();
+        JLgenero2 = new javax.swing.JLabel();
+        JLfoto3 = new javax.swing.JLabel();
+        JLtitulo3 = new javax.swing.JLabel();
+        JLgenero3 = new javax.swing.JLabel();
+        JLfoto4 = new javax.swing.JLabel();
+        JLtitulo4 = new javax.swing.JLabel();
+        JLgenero4 = new javax.swing.JLabel();
+        JLfoto5 = new javax.swing.JLabel();
+        JLtitulo5 = new javax.swing.JLabel();
+        JLgenero5 = new javax.swing.JLabel();
+        JLfoto6 = new javax.swing.JLabel();
+        JLtitulo6 = new javax.swing.JLabel();
+        JLgenero6 = new javax.swing.JLabel();
+        JLfoto7 = new javax.swing.JLabel();
+        JLtitulo7 = new javax.swing.JLabel();
+        JLgenero7 = new javax.swing.JLabel();
+        JLfoto8 = new javax.swing.JLabel();
+        JLtitulo8 = new javax.swing.JLabel();
+        JLgenero8 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Technology");
+        setMinimumSize(new java.awt.Dimension(770, 720));
+        setPreferredSize(new java.awt.Dimension(770, 720));
+
+        JScroll1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        JScroll1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        JPanel.setLayout(new java.awt.GridBagLayout());
+
+        JLfoto1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/aprendices_fuera_de_linea.jpg"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLfoto1, gridBagConstraints);
+
+        JLtitulo1.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
+        JLtitulo1.setText("Titulo: Aprendices fuera de línea");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 20);
+        JPanel.add(JLtitulo1, gridBagConstraints);
+
+        JLgenero1.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 15)); // NOI18N
+        JLgenero1.setText("Género: Comedia");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 0, 0);
+        JPanel.add(JLgenero1, gridBagConstraints);
+
+        JLfoto2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/red_social.jpg"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLfoto2, gridBagConstraints);
+
+        JLtitulo2.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
+        JLtitulo2.setText("Titulo: La Red Social");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 20);
+        JPanel.add(JLtitulo2, gridBagConstraints);
+
+        JLgenero2.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 15)); // NOI18N
+        JLgenero2.setText("Género: Drama");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 0, 0);
+        JPanel.add(JLgenero2, gridBagConstraints);
+
+        JLfoto3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/jobs.jpg"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLfoto3, gridBagConstraints);
+
+        JLtitulo3.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
+        JLtitulo3.setText("Titulo: Jobs");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 20);
+        JPanel.add(JLtitulo3, gridBagConstraints);
+
+        JLgenero3.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 15)); // NOI18N
+        JLgenero3.setText("Géneo: Biografía, Drama");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 5;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 0, 0);
+        JPanel.add(JLgenero3, gridBagConstraints);
+
+        JLfoto4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/dilema_de_las_redes_sociales.jpg"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 6;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLfoto4, gridBagConstraints);
+
+        JLtitulo4.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
+        JLtitulo4.setText("Titulo: El dilema de las redes sociales");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 6;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLtitulo4, gridBagConstraints);
+
+        JLgenero4.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 15)); // NOI18N
+        JLgenero4.setText("Género: Documental");
+        JLgenero4.setToolTipText("");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 7;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 0, 0);
+        JPanel.add(JLgenero4, gridBagConstraints);
+
+        JLfoto5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/codigo_de_la_discordia.jpg"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 8;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLfoto5, gridBagConstraints);
+
+        JLtitulo5.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
+        JLtitulo5.setText("Titulo: El Código De La Discordia");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 8;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLtitulo5, gridBagConstraints);
+
+        JLgenero5.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 15)); // NOI18N
+        JLgenero5.setText("Género: Drama");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 9;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 0, 0);
+        JPanel.add(JLgenero5, gridBagConstraints);
+
+        JLfoto6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/codigo_enigma.jpg"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 10;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLfoto6, gridBagConstraints);
+
+        JLtitulo6.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
+        JLtitulo6.setText("Titulo: El Código Enigma");
+        JLtitulo6.setToolTipText("");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 10;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLtitulo6, gridBagConstraints);
+
+        JLgenero6.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 15)); // NOI18N
+        JLgenero6.setText("Género: Biografía, Drama, Suspenso, Bélico ");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 11;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 0, 0);
+        JPanel.add(JLgenero6, gridBagConstraints);
+
+        JLfoto7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/mr_robot.jpg"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 12;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLfoto7, gridBagConstraints);
+
+        JLtitulo7.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
+        JLtitulo7.setText("Titulo: Mr. Robot");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 12;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLtitulo7, gridBagConstraints);
+
+        JLgenero7.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 15)); // NOI18N
+        JLgenero7.setText("Género: Crimen, Drama, Suspenso");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 13;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 0, 0);
+        JPanel.add(JLgenero7, gridBagConstraints);
+
+        JLfoto8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/snowden.jpg"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 14;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 90, 0);
+        JPanel.add(JLfoto8, gridBagConstraints);
+
+        JLtitulo8.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 18)); // NOI18N
+        JLtitulo8.setText("Titulo: Snowden");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 14;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 0, 0);
+        JPanel.add(JLtitulo8, gridBagConstraints);
+
+        JLgenero8.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 15)); // NOI18N
+        JLgenero8.setText("Género: Biografía, Crimen, Drama, Suspenso");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 15;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.weightx = 0.5;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 0, 0);
+        JPanel.add(JLgenero8, gridBagConstraints);
+
+        JScroll1.setViewportView(JPanel);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(JScroll1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 639, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(JScroll1, javax.swing.GroupLayout.PREFERRED_SIZE, 769, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Multimedia().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel JLfoto1;
+    private javax.swing.JLabel JLfoto2;
+    private javax.swing.JLabel JLfoto3;
+    private javax.swing.JLabel JLfoto4;
+    private javax.swing.JLabel JLfoto5;
+    private javax.swing.JLabel JLfoto6;
+    private javax.swing.JLabel JLfoto7;
+    private javax.swing.JLabel JLfoto8;
+    private javax.swing.JLabel JLgenero1;
+    private javax.swing.JLabel JLgenero2;
+    private javax.swing.JLabel JLgenero3;
+    private javax.swing.JLabel JLgenero4;
+    private javax.swing.JLabel JLgenero5;
+    private javax.swing.JLabel JLgenero6;
+    private javax.swing.JLabel JLgenero7;
+    private javax.swing.JLabel JLgenero8;
+    private javax.swing.JLabel JLtitulo1;
+    private javax.swing.JLabel JLtitulo2;
+    private javax.swing.JLabel JLtitulo3;
+    private javax.swing.JLabel JLtitulo4;
+    private javax.swing.JLabel JLtitulo5;
+    private javax.swing.JLabel JLtitulo6;
+    private javax.swing.JLabel JLtitulo7;
+    private javax.swing.JLabel JLtitulo8;
+    private javax.swing.JPanel JPanel;
+    private javax.swing.JScrollPane JScroll1;
+    // End of variables declaration//GEN-END:variables
+}
